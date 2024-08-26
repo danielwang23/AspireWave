ebe66ac68f2aefde3bde67fc2dd62bef05df2d72
Daniel Wang <dalbertw@unc.edu>
2024-08-25 21:47:25 -0400

    Initial Commit

---
+++ AspireWave/AspireWaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  AspireWaveApp.swift
+//  AspireWave
+//
+//  Created by Daniel Wang on 8/25/24.
+//
+
+import SwiftUI
+
+@main
+struct AspireWaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AspireWave/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AspireWave
+//
+//  Created by Daniel Wang on 8/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
