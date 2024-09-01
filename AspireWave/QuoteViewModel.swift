66ea8c97e7e4eced83b6ed18982a3ab44697e015
Daniel Wang <dalbertw@unc.edu>
2024-09-01 17:53:42 -0400

    Revert "updates to scroll nav"
    This reverts commit 315f1e08cfc5773a1971d4912829fc2c95c871d1.
    
    revert tttttt branch is up to date with 'origin/main'.

--- AspireWave/QuoteViewModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  QuoteViewModel.swift
-//  AspireWave
-//
-//  Created by Daniel Wang on 8/30/24.
-//
-
-import Foundation
--- AspireWave/Quotes.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Quotes.swift
-//  AspireWave
-//
-//  Created by Daniel Wang on 8/30/24.
-//
-
-import Foundation
